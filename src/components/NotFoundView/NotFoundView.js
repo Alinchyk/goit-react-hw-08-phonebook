024da138a949ebd0b7f1ecc106bbc71f262564ce
Alinchyk <alina.mykhalchuk09@gmail.com>
2023-07-23 17:39:01 +0200

    Update NotFoundView.js

--- src/components/NotFoundView/NotFoundView.js
+++ src/components/NotFoundView/NotFoundView.js
@@ -1,7 +1,5 @@
 export default function NotFoundView() {
   return (
-    <h1 style={{ marginTop: 100, textAlign: 'center' }}>
-      404 Page not found :(
-    </h1>
+    <h1 style={{ marginTop: 100, textAlign: 'center' }}>404 Page not found</h1>
   );
 }
